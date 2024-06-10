529e51dfb4081ab7844068db1b9c381b2021b575
xyaoh <grangeanthony@pm.me>
2024-06-10 03:25:24 -0400

    gruvbox-material

--- config/colorscheme.nix
+++ config/colorscheme.nix
@@ -1,6 +1,28 @@
+{ self, pkgs, ... }:
+
 {
-  colorschemes.base16 = {
-    enable = true;
-    colorscheme = "gruvbox-material-dark-medium";
+  extraPlugins = with pkgs.vimPlugins; [ 
+    # https://github.com/sainnhe/gruvbox-material
+    gruvbox-material
+  ];
+
+  globals = {
+    gruvbox_material_background = "medium";
+    gruvbox_material_foreground = "material";
+    gruvbox_material_disable_italic_comment = 0;
+    gruvbox_material_enable_bold = 1;
+    gruvbox_material_enable_italic = 1;
+    gruvbox_material_visual = "reverse";
+    gruvbox_material_menu_selection_background = "orange";
+    gruvbox_material_sign_column_background = "none";
+    gruvbox_material_spell_foreground = "colored";
+    gruvbox_material_ui_contrast = "low";
+    gruvbox_material_show_eob = 1;
+    gruvbox_material_diagnostic_text_highlight = 0;
+    gruvbox_material_diagnostic_line_highlight = 0;
+    gruvbox_material_diagnostic_virtual_text = "colored";
+    gruvbox_material_current_word = "underline";
   };
+
+  colorscheme = "gruvbox-material";
 }
