0c8fd1cc2b76511becf263a2b8107617ff54107b
xyaoh <grangeanthony@pm.me>
2024-06-12 19:55:38 -0400

    python lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -3,12 +3,16 @@
     lsp = {
       enable = true;
       servers = {
+        # rust
         rust-analyzer = {
           enable = true;
           installCargo = false;
           installRustc = false;
         };
+        # haskell
         hls.enable = true;
+        # python
+        ruff.enable = true;
       };
     };
   };
