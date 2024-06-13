59eb9dcc21f655906a7d5dd572cbe0a7d730d05d
xyaoh <grangeanthony@pm.me>
2024-06-12 20:28:55 -0400

    some more options

--- config/options.nix
+++ config/options.nix
@@ -5,12 +5,22 @@
     number = true;
     relativenumber = true;
 
+    splitbelow = true; # new window is put below the current one
+    splitright = true; # new window is put right of the current one
+
     shiftwidth = 2;
     tabstop = 2;
-    softtabstop = 2;
     expandtab = true;
 
     ignorecase = true;
-    scrolloff = 10;
+    smartcase = true; # override 'ignorecase' option if the search pattern
+                      # contains upper case characters
+
+    scrolloff = 10; # minimum number of screen lines to show around the cursor
+
+    termguicolors = true; # enables 24-bit RGB color in the |TUI|
+
+    swapfile = false; # disable the swap file
+    undofile = true; # automatically save and restore undo history
   };
 }
