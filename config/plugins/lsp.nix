e7a863fe9c091b37f1bdc32bdf94ae97e8724ef5
xyaoh <grangeanthony@pm.me>
2024-06-12 22:43:26 -0400

    lsp basic keymaps

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -2,6 +2,23 @@
   plugins = {
     lsp = {
       enable = true;
+
+      keymaps = {
+        silent = true;
+        diagnostic = {
+          "<leader>k" = "goto_prev";
+          "<leader>j" = "goto_next";
+        };
+        lspBuf = {
+          gd = "definition";
+          gD = "references";
+          gt = "type_definition";
+          gi = "implementation";
+          K = "hover";
+          "<F2>" = "rename";
+        };
+      };
+
       servers = {
         # rust
         rust-analyzer = {
