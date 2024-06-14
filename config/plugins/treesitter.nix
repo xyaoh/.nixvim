3ad38362b025aefc905de5d711f936e97a2578cb
xyaoh <grangeanthony@pm.me>
2024-06-13 21:35:25 -0400

    ts refactor / context , indent opt, hmts

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -2,6 +2,21 @@
   plugins = {
     treesitter = {
       enable = true;
+      indent = true;
     };
+
+    treesitter-refactor = {
+      enable = true;
+      highlightDefinitions = {
+        enable = true;
+      };
+    };
+
+    treesitter-context = {
+      enable = true;
+    };
+
+    # highlight languages inside strings for nix files
+    hmts.enable = true;
   };
 }
