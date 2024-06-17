7103e63673914f609fdf1e192ac6a58766a90512
xyaoh <grangeanthony@pm.me>
2024-06-17 08:29:56 -0400

    few visual settings for oil

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,5 +1,32 @@
 {
   plugins.oil = {
     enable = true;
+
+    settings = {
+      columns = [
+        "icon"
+        "permissions"
+      ];
+
+      win_options = {
+        signcolumn = "yes";
+      };
+
+      keymaps_help = {
+        border = "none";
+      };
+      float = {
+        border = "none";
+      };
+      preview = {
+        border = "none";
+      };
+      progress = {
+        border = "none";
+      };
+      ssh = {
+        border = "none";
+      };
+    };
   };
 }
