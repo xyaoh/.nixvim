43dc9293aefed8b80a53722cc1206784c830d56f
xyaoh <grangeanthony@pm.me>
2024-06-16 20:38:52 -0400

    refactor plugins import

--- config/default.nix
+++ config/default.nix
@@ -3,14 +3,6 @@
     ./options.nix
     ./keymaps.nix
     ./colorscheme.nix
-    ./plugins/indent-blankline.nix
-    ./plugins/treesitter.nix
-    ./plugins/lsp.nix
-    ./plugins/barbar.nix
-    ./plugins/oil.nix
-    ./plugins/lualine.nix
-    ./plugins/alpha.nix
-    ./plugins/fugitive.nix
-    ./plugins/telescope.nix
+    ./plugins
   ];
 }
---
+++ config/plugins/default.nix
@@ -0,0 +1,16 @@
+{
+  imports = [
+    ./alpha.nix
+    ./lualine.nix
+    ./barbar.nix
+    ./indent-blankline.nix
+
+    ./treesitter.nix
+    ./lsp.nix
+
+    ./oil.nix
+    ./telescope.nix
+
+    ./fugitive.nix
+  ];
+}
