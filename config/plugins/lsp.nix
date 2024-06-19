51992b9c55978d4139cb917792c1de978f86f9a6
xyaoh <grangeanthony@pm.me>
2024-06-19 12:19:39 -0400

    lsp refactor / keymap change

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,36 +1,34 @@
 {
-  plugins = {
-    lsp = {
-      enable = true;
+  plugins.lsp = {
+    enable = true;
 
-      keymaps = {
-        silent = true;
-        diagnostic = {
-          "<leader>k" = "goto_prev";
-          "<leader>j" = "goto_next";
-        };
-        lspBuf = {
-          gd = "definition";
-          gD = "references";
-          gt = "type_definition";
-          gi = "implementation";
-          K = "hover";
-          "<F2>" = "rename";
-        };
+    keymaps = {
+      silent = true;
+      diagnostic = {
+        "<leader>k" = "goto_prev";
+        "<leader>j" = "goto_next";
       };
+      lspBuf = {
+        gd = "definition";
+        gr = "references";
+        gt = "type_definition";
+        gi = "implementation";
+        K = "hover";
+        "<F2>" = "rename";
+      };
+    };
 
-      servers = {
-        # rust
-        rust-analyzer = {
-          enable = true;
-          installCargo = false;
-          installRustc = false;
-        };
-        # haskell
-        hls.enable = true;
-        # python
-        ruff.enable = true;
+    servers = {
+      # rust
+      rust-analyzer = {
+        enable = true;
+        installCargo = false;
+        installRustc = false;
       };
+      # haskell
+      hls.enable = true;
+      # python
+      ruff.enable = true;
     };
   };
 }
