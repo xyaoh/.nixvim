6715475eae60c1098213994a778620f0cad6df9d
xyaoh <grangeanthony@pm.me>
2024-06-19 12:10:51 -0400

    rm TS-refactor and TS-context

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -5,25 +5,7 @@
       indent = true;
     };
 
-    treesitter-refactor = {
-      enable = true;
-      highlightDefinitions = {
-        enable = true;
-      };
-    };
-
-    treesitter-context = {
-      enable = true;
-      settings = {
-        max_lines = 4;
-      };
-    };
-
     # highlight languages inside strings for nix files
     hmts.enable = true;
-  };
-
-  highlightOverride = {
-    TSDefinition.link = "Visual";
   };
 }
